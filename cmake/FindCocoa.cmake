0d01c8e627461db99f1ac5e8f10d067903ef1a4e
Frederic Simonis <simonisfrederic@gmail.com>
2018-11-11 23:33:44 +0100

    Tidy CMakeLists and fix Cocoa

--- cmake/FindCocoa.cmake
+++ cmake/FindCocoa.cmake
@@ -14,15 +14,21 @@ endif()
 find_path(Cocoa_INCLUDE_DIRS NAMES Cocoa.h HINTS ${PC_Cocoa_INCLUDE_DIRS} PATH_SUFFIXES Cocoa)
 find_library(Cocoa_LIBRARIES NAMES Cocoa HINTS ${PC_Cocoa_LIBRARY_DIRS})
 
+set(Cocoa_VERSION ${PC_Cocoa_VERSION})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Cocoa DEFAULT_MSG Cocoa_LIBRARIES Cocoa_INCLUDE_DIRS)
+find_package_handle_standard_args(Cocoa 
+    REQUIRED_VARS Cocoa_LIBRARIES Cocoa_INCLUDE_DIRS
+    VERSION_VAR Cocoa_VERSION
+    )
 
 if(Cocoa_FOUND)
     if(NOT TARGET Cocoa::Cocoa)
         add_library(Cocoa::Cocoa UNKNOWN IMPORTED)
-        target_include_directories(Cocoa::Cocoa INTERFACE ${Cocoa_INCLUDE_DIRS})
-        target_link_libraries(Cocoa::Cocoa INTERFACE ${Cocoa_LIBRARIES})
+        set_target_properties(Cocoa::Cocoa PROPERTIES
+            INTERFACE_LINK_LIBRARIES ${Cocoa_LIBRARIES}
+            INTERFACE_INCLUDE_DIR ${Cocoa_INCLUDE_DIRS}
+            )
     endif()
 endif()
 
